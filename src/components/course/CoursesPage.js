cd4f6f5e3567cbcb9fa60924120607d8c9e1d0ad
harit <harit.himanshu@yahoo.in>
2016-09-07 17:14:54 -0700

    Some Components with Router setup

---
+++ src/components/course/CoursesPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class CoursesPage extends React.Component {
+  render() {
+    return <div>
+      <h1>Courses Page</h1>
+    </div>
+  }
+}
+
+export default CoursesPage;
