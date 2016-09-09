d29c13e9c7081a031f20b62e9841d623debe524f
harit <harit.himanshu@yahoo.in>
2016-09-09 13:30:53 -0700

    completing redux flow

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@
 import 'babel-polyfill';
 import React from 'react';
 import {render} from 'react-dom';
-/*import configureStore from './store/configureStore';*/
+import configureStore from './store/configureStore';
 import {Provider} from 'react-redux';
 import {Router, browserHistory} from 'react-router';
 import routes from './routes';
@@ -15,13 +15,14 @@ import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import '../node_modules/toastr/build/toastr.min.css';
 
 /*
- const store = configureStore();
  store.dispatch(loadCourses());
  store.dispatch(loadAuthors());
  */
+const store = configureStore();
 
 render(
-  <Router history={browserHistory} routes={routes}/>,
+  <Provider store={store}>
+    <Router history={browserHistory} routes={routes}/>
+  </Provider>,
   document.getElementById('app')
-)
-;
+);
