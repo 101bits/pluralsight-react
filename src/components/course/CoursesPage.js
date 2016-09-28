1fe81a1ba2cbebdb07925a3e8de6a9a3c31a6413
harit <harit.himanshu@yahoo.in>
2016-09-28 13:08:23 -0700

    manage Courses Page. next? Async status and Error handling

--- src/components/course/CoursesPage.js
+++ src/components/course/CoursesPage.js
@@ -4,19 +4,31 @@ import * as courseActions from '../../actions/courseActions';
 import CourseList from './CourseList';
 import * as PropTypes from "react/lib/ReactPropTypes";
 import {bindActionCreators} from 'redux';
+import {browserHistory} from 'react-router';
 
 class CoursesPage extends React.Component {
   constructor(props, context) {
     super(props, context);
+
+    this.redirectToAddCoursePage = this.redirectToAddCoursePage.bind(this);
   }
 
   courseRow(course, index) {
     return <div key={index}>{course.title}</div>;
   }
 
+  redirectToAddCoursePage() {
+    browserHistory.push('/course');
+  }
+
   render() {
     return (<div>
         <h1>Courses</h1>
+        <input type="submit"
+               value="Add Course"
+               className="btn btn-primary"
+               onClick={this.redirectToAddCoursePage}/>
+
         <CourseList courses={this.props.courses}/>
       </div>
     );
